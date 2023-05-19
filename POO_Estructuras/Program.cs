2e7c2c74c8404fe8b34dbe24ccf26f89b823f288
Alfredo Casanova <maattos07@outlook.com>
2023-05-19 09:31:35 -0500

    Agregar archivos de proyecto.

---
+++ POO_Estructuras/Program.cs
@@ -0,0 +1,17 @@
+﻿using POO_Estructuras.Servicios;
+using System;
+
+namespace POO_Estructuras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AlumnoServicio alumnoservicio = new AlumnoServicio();
+
+            var res = alumnoservicio.Agregar();
+            alumnoservicio.Imprimir(res);
+            Console.WriteLine("La calificacion es "+ alumnoservicio.ValidarProm(res.Promedio);
+        }
+    }
+}
---
+++ POO_Estructuras/Servicios/AlumnoServicio.cs
@@ -0,0 +1,65 @@
+﻿using POO_Estructuras.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Estructuras.Servicios
+{
+    public class AlumnoServicio
+    {
+        public Alumno Agregar()
+        {
+            try
+            {
+                Alumno alumno2 = new Alumno()
+                {
+                Id = 2,
+                Nombre = "Alfredo",
+                Apellido = "Casanova",
+                Fecha = DateTime.Now,
+                Grado = 3,
+                Promedio = 9.5
+                };
+                return alumno2;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Sucedio un error " + ex.Message);
+            }
+        }
+
+        public void Imprimir(Alumno request)
+        {
+            try
+            {
+                Console.WriteLine("El nombre es: "+request.Nombre);
+                Console.WriteLine("El apellido es: "+request.Apellido);
+                Console.WriteLine("Ingreso: "+request.Fecha);
+                Console.WriteLine("Esta en el grado: "+request.Grado);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Sucedio un error "+ ex.Message);
+            }
+        }
+
+        internal void Imprimir(object request)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region ------Validar aprobacion
+        public bool ValidarProm(double num1)
+        {
+            bool validar = false;
+            if (num1 >= 7)
+            {
+                validar = true;
+            }
+            return validar;
+        }
+        #endregion
+    }
+}
